2522307385633cabe0ba578900f04716993eb7f4
Akash <akashbspandey01@gmail.com>
2026-09-12 15:18:57 +0530

    Configure Upstash Redis

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -4,6 +4,7 @@ export const redis = new Redis({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
     password: process.env.REDIS_PASSWORD,
+    tls: {},
     maxRetriesPerRequest: null,
 });
 
